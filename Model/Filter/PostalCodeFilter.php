c0d65853a900a6a3f9f6f731889631cc354ba967
Nickolas Burr <nickolasburr@gmail.com>
2020-01-16 01:46:20 -0500

    Add PostalCodeFilter model

---
+++ Model/Filter/PostalCodeFilter.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * PostalCodeFilter.php
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License, which
+ * is bundled with this package in the file LICENSE.txt.
+ *
+ * It is also available on the Internet at the following URL:
+ * https://docs.auroraextensions.com/magento/extensions/2.x/shippingfilters/LICENSE.txt
+ *
+ * @package       AuroraExtensions_ShippingFilters
+ * @copyright     Copyright (C) 2020 Aurora Extensions <support@auroraextensions.com>
+ * @license       MIT License
+ */
+declare(strict_types=1);
+
+namespace AuroraExtensions\ShippingFilters\Model\Filter;
+
+use AuroraExtensions\ShippingFilters\{
+    Component\System\ModuleConfigTrait,
+    Csi\Filter\CountryFilterInterface,
+    Csi\Filter\PostalCodeFilterInterface,
+    Csi\Filter\RegionFilterInterface,
+    Csi\System\ModuleConfigInterface,
+    Model\ResourceModel\PostalCode\Collection,
+    Model\ResourceModel\PostalCode\CollectionFactory
+};
+use Magento\Store\Model\StoreManagerInterface;
+
+class PostalCodeFilter implements PostalCodeFilterInterface
+{
+    /**
+     * @property ModuleConfigInterface $moduleConfig
+     * @method ModuleConfigInterface getModuleConfig()
+     */
+    use ModuleConfigTrait;
+
+    /** @property CollectionFactory $collectionFactory */
+    protected $collectionFactory;
+
+    /** @property CountryFilterInterface $countryFilter */
+    protected $countryFilter;
+
+    /** @property RegionFilterInterface $regionFilter */
+    protected $regionFilter;
+
+    /** @property StoreManagerInterface $storeManager */
+    protected $storeManager;
+
+    /**
+     * @param CollectionFactory $collectionFactory
+     * @param CountryFilterInterface $countryFilter
+     * @param ModuleConfigInterface $moduleConfig
+     * @param RegionFilterInterface $regionFilter
+     * @param StoreManagerInterface $storeManager
+     * @return void
+     */
+    public function __construct(
+        CollectionFactory $collectionFactory,
+        CountryFilterInterface $countryFilter,
+        ModuleConfigInterface $moduleConfig,
+        RegionFilterInterface $regionFilter,
+        StoreManagerInterface $storeManager
+    ) {
+        $this->collectionFactory = $collectionFactory;
+        $this->countryFilter = $countryFilter;
+        $this->moduleConfig = $moduleConfig;
+        $this->regionFilter = $regionFilter;
+        $this->storeManager = $storeManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPostalCodes(): array
+    {
+        /** @var StoreInterface $store */
+        $store = $this->storeManager->getStore();
+
+        /** @var string|null $whitelist */
+        $whitelist = $this->getModuleConfig()
+            ->getPostalCodeWhitelist((int) $store->getId());
+
+        /** @var array $postalCodes */
+        $postalCodes = array_filter(
+            array_map(
+                'trim',
+                explode(',', $whitelist)
+            ),
+            'strlen'
+        );
+
+        return $postalCodes;
+    }
+
+    /**
+     * @param string $code
+     * @return array
+     */
+    public function getOptionsByRegionCode(string $code): array
+    {
+        /** @var array $countries */
+        $countries = $this->countryFilter
+            ->getCountries();
+
+        /** @var Collection $postalCodes */
+        $postalCodes = $this->collectionFactory
+            ->create()
+            ->addCountriesFilter($countries)
+            ->addRegionFilter($code)
+            ->addFieldToFilter(
+                'main_table.postal_code_id',
+                ['in' => $this->getPostalCodes()]
+            )
+            ->load();
+
+        return $postalCodes->toOptionArray();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOptions(): array
+    {
+        /** @var array $countries */
+        $countries = $this->countryFilter
+            ->getCountries();
+
+        /** @var array $regions */
+        $regions = $this->regionFilter
+            ->getRegions();
+
+        /** @var Collection $postalCodes */
+        $postalCodes = $this->collectionFactory
+            ->create()
+            ->addCountriesFilter($countries)
+            ->addRegionsFilter($regions)
+            ->addFieldToFilter(
+                'main_table.postal_code_id',
+                ['in' => $this->getPostalCodes()]
+            )
+            ->load();
+
+        return $postalCodes->toOptionArray();
+    }
+}
