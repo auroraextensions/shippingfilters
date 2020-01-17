cff9dea2c1e974d693825c0d651e2006ae5562ae
Nickolas Burr <nickolasburr@gmail.com>
2020-01-17 14:04:57 -0500

    Update AddFilterShippingRegionDictionary plugin namespace

--- Plugin/Checkout/Region/AddFilterShippingRegionDictionary.php
+++ Plugin/Checkout/Region/AddFilterShippingRegionDictionary.php
@@ -16,7 +16,7 @@
  */
 declare(strict_types=1);
 
-namespace AuroraExtensions\ShippingFilters\Plugin\Checkout;
+namespace AuroraExtensions\ShippingFilters\Plugin\Checkout\Region;
 
 use AuroraExtensions\ShippingFilters\Csi\Filter\RegionFilterInterface;
 use Magento\Checkout\Block\Checkout\LayoutProcessorInterface;
