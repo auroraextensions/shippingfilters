9d3c050e0fad4dd33e7084e092f374317bbb87b0
Nickolas Burr <nickolasburr@gmail.com>
2020-01-22 16:29:13 -0500

    Work on option filters in city.js, postal-code.js

--- view/frontend/web/js/form/element/city.js
+++ view/frontend/web/js/form/element/city.js
@@ -17,9 +17,19 @@ define([
     'underscore',
     'ko',
     'uiRegistry',
+    'mage/translate',
     'Magento_Ui/js/form/element/select'
-], function (_, ko, registry, Select) {
+], function (_, ko, registry, $t, Select) {
     'use strict';
+
+    /**
+     * @param {Object} a
+     * @param {Object} b
+     * @return {Boolean}
+     */
+    function isCompositeEqual(a, b) {
+        return (JSON.stringify(a) === JSON.stringify(b));
+    }
 
     return Select.extend({
         defaults: {
@@ -27,32 +37,59 @@ define([
             exports: {
                 locality: '${ $.parentName }.city:value'
             },
+            filterOptions: ko.observableArray([]),
+            imports: {
+                onRegionSelect: '${ $.parentName }.region_id:value'
+            },
+            initialized: ko.observable(false),
             listens: {
+                options: 'onOptionsChange',
                 value: 'onValueChange'
             },
-            locality: ko.observable()
+            locality: ko.observable(),
+            messages: {
+                warnings: {
+                    noOptionsAvailable: $t('We\'re unable to ship to your selected city. We apologize for the inconvenience.'),
+                    oneOptionAvailable: $t('The selected city is the only available option. We apologize for any inconvenience.')
+                }
+            }
+        },
+        /**
+         * {@inheritdoc}
+         */
+        initialize: function () {
+            this._super();
+
+            this.filterOptions(this.initialOptions);
+            this.setOptions(this.initialOptions);
+            this.initialized(true);
+
+            return this;
         },
         /**
          * @param {mixed} value
          * @param {String} field
+         * @param {String} dict
          * @return {mixed}
          */
-        getFieldByValue: function (value, field) {
-            var dict, index,
+        getFieldByValue: function (value, field, dict) {
+            var data, index,
                 item, result = null;
 
-            registry.get(this.provider, function (provider) {
-                /** @var {Array} dict */
-                dict = provider.dictionaries[this.dict];
+            dict = dict || this.dict;
 
-                if (!dict) {
+            registry.get(this.provider, function (provider) {
+                /** @var {Array} data */
+                data = provider.dictionaries[dict];
+
+                if (!data) {
                     return result;
                 }
 
                 /** @var {Number} index */
-                for (index = 0; index < dict.length; index += 1) {
+                for (index = 0; index < data.length; index += 1) {
                     /** @var {Object} item */
-                    item = dict[index];
+                    item = data[index];
 
                     if (result !== null) {
                         break;
@@ -70,6 +107,71 @@ define([
          * @param {String} value
          * @return {void}
          */
+        onRegionSelect: function (value) {
+            var result;
+
+            if (!value) {
+                return;
+            }
+
+            /** @var {Array} result */
+            result = this.filterByRegion(value);
+
+            this.filterOptions(result);
+            this.setOptions(result);
+
+            if (!result.length) {
+                this.disabled(true);
+                this.error(false);
+                this.warn(this.messages.warnings['noOptionsAvailable']);
+            } else if (result.length < 2) {
+                this.disabled(true);
+                this.value(result[0]['value']);
+                this.warn(this.messages.warnings['oneOptionAvailable']);
+            } else {
+                this.disabled(false);
+                this.warn(false);
+            }
+        },
+        /**
+         * @param {Array} options
+         * @return {void}
+         */
+        onOptionsChange: function (options) {
+            if (!this.initialized()) {
+                return;
+            }
+
+            if (!isCompositeEqual(options, this.filterOptions())) {
+                this.setOptions(this.filterOptions());
+            }
+        },
+        /**
+         * @param {String} value
+         * @return {Array}
+         */
+        filterByRegion: function (value) {
+            var field, region;
+
+            if (!value) {
+                return [];
+            }
+
+            /** @var {Number} region */
+            region = +(this.getFieldByValue(
+                value,
+                'value',
+                'whitelist_region_id'
+            ));
+
+            return _.filter(this.initialOptions, function (item) {
+                return (item['region_id'] === region || item.value === '');
+            });
+        },
+        /**
+         * @param {String} value
+         * @return {void}
+         */
         onValueChange: function (value) {
             var name;
 
@@ -82,7 +184,6 @@ define([
                 value,
                 'locality_name'
             );
-
             this.locality(name);
         }
     });
